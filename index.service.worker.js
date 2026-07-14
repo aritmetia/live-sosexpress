56d5ddf086291efdf0e249f34b31af3e1b0285a6
ARX <adriancamposcr@gmail.com>
2026-07-14 11:18:18 -0600

    0.1.3

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783450371|20044620';
+const CACHE_VERSION = '1784049471|188828004';
 /** @type {string} */
 const CACHE_PREFIX = 'Aritmetia - SOS -sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
